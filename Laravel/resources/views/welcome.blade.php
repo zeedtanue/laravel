581db45ef140f3851399b3c234a7f08f50a74ea1
zeedtanue <zeedtanue@gmail.com>
2019-12-24 03:40:39 +0800

    welcome page change

--- Laravel/resources/views/welcome.blade.php
+++ Laravel/resources/views/welcome.blade.php
@@ -81,18 +81,18 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel
+                    
+                    <h3>Laravel Assesment Project</h3>
+                    
                 </div>
 
                 <div class="links">
-                    <a href="https://laravel.com/docs">Docs</a>
-                    <a href="https://laracasts.com">Laracasts</a>
-                    <a href="https://laravel-news.com">News</a>
-                    <a href="https://blog.laravel.com">Blog</a>
-                    <a href="https://nova.laravel.com">Nova</a>
-                    <a href="https://forge.laravel.com">Forge</a>
-                    <a href="https://vapor.laravel.com">Vapor</a>
-                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                    <strong> 5 Objectives:</strong>
+                    <a href="/login">1.Login</a>
+                    <a href="/register">2.Registration</a>
+                    <a href="/user/profile">3.User Crud</a>
+                    <a href="/company/profile">4.Company Crud</a>
+                    <a href="/employee/details">5.Employee Crud</a>
                 </div>
             </div>
         </div>
